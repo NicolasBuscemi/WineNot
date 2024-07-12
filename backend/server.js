bba65ee9d3a6cb56eb3f7123f958b4e066ddbd12
NicolasBuscemi <91195492+NicolasBuscemi@users.noreply.github.com>
2024-07-12 18:06:43 +0200

    feat(api): Implement initial GET request handling

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,29 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const wineRoutes = require('./routes/wineRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-const mongoURI = 'mongodb+srv://busceminicolassa:in78W2Mn0otRFWda@cluster0.zhnqvsy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const PORT = process.env.PORT || 3001;
+const mongoURI = process.env.MONGO_URI;
 
 mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 }).then(() => {
-  console.log('MongoDB connected');
+    console.log('MongoDB connected to db');
 }).catch(err => {
-  console.error('Error connecting to MongoDB', err);
+    console.error('Error connecting to MongoDB', err);
 });
 
 app.use(express.json());
 app.use('/api', wineRoutes);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
