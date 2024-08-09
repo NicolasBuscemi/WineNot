4bd9f75f97da5cbc9c7d3b93ebcaa4c8294a1ace
NicolasBuscemi <91195492+NicolasBuscemi@users.noreply.github.com>
2024-08-09 18:26:30 +0200

    Remove deprecated MongoDB connection options: useNewUrlParser and useUnifiedTopology

--- backend/server.js
+++ backend/server.js
@@ -35,8 +35,7 @@ app.use((req, res, next) => {
 });
 
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  
 }).then(() => {
     console.log('MongoDB connected to db');
 }).catch((error) => {
